223dd25ac1f12a818bab01f375472db821fcbf0f
sumesht30 <sumesh.t95@gmail.com>
2022-03-31 13:25:19 +0530

    anu done her work

--- src/org/sample/EmployeeDetails.java
+++ src/org/sample/EmployeeDetails.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class EmployeeDetails {
+	private void m1() {
+		System.out.println("m1  method created by anu");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
