909036bfaa792f965105bc09c8b6ae912ee5a188
sumesht30 <sumesh.t95@gmail.com>
2022-03-31 13:43:24 +0530

    done

--- src/org/sample/EmployeeDetails.java
+++ src/org/sample/EmployeeDetails.java
@@ -1,9 +1,13 @@
 package org.sample;
 
 public class EmployeeDetails {
+	private void m3() {
+		System.out.println("m3 method created by sandy");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
+		
 	}
 
 }
