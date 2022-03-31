ae48521c1f400c3eac0caa9b17de369867509cbb
sumesht30 <sumesh.t95@gmail.com>
2022-03-31 13:31:29 +0530

    dinesh done his work

--- src/org/sample/EmployeeDetails.java
+++ src/org/sample/EmployeeDetails.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class EmployeeDetails {
+	private void m2() {
+		System.out.println("m2 method created by dinesh");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
